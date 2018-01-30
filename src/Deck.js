3d57b1745b55a1ec32bf6878aac1e3edd4082bb3
Fuad Balashov <fuad.balashov@appian.com>
2018-01-30 11:52:13 -0500

    Add gesture animations

--- src/Deck.js
+++ src/Deck.js
@@ -9,16 +9,31 @@ class Deck extends React.Component {
   constructor(props) {
     super(props);
 
+    const position = new Animated.ValueXY();
     const panResponder = PanResponder.create({
-
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => {
+        position.setValue({x: gesture.dx, y: gesture.dy});
+      },
+      onPanResponderRelease: () => {}
     });
 
-    this.state = { panResponder };
+    this.state = { panResponder, position };
   }
 
   renderCards() {
-    return this.props.data.map(element => {
-        return this.props.renderCard(element);
+    return this.props.data.map((element, index) => {
+      if (index === 0) {
+        return (
+          <Animated.View
+            style={this.state.position.getLayout()}
+            { ...this.state.panResponder.panHandlers }
+          >
+            {this.props.renderCard(element)}
+          </Animated.View>
+        );
+      }
+      return this.props.renderCard(element);
     });
   }
 
